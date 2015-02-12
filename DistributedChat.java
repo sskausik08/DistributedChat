22bc6c98f37f26fda5c2b51e50356982e15f3a0a
Kausik Subramanian <sskausik08@gmail.com>
2015-02-12 15:58:51 +0530

    Join working

--- DistributedChat.java
+++ DistributedChat.java
@@ -20,23 +20,7 @@ public class DistributedChat {
 		ID = id;
 		inputSc = new Scanner(System.in);
 
-		try {
-			mSocket = new MulticastSocket(Port);
-		} catch (Exception e) {
-			System.err.println("Exception in 1");
-		}
-	    try { 
-			MulticastAddress = InetAddress.getByName("225.50.4.1");
-		} catch (java.net.UnknownHostException e) {
-			System.err.println("Unknown Host Reported");
-		}
-		try{
-			SocketAddress socketAddress = new InetSocketAddress(MulticastAddress, 9999);
-			NetworkInterface networkInterface = NetworkInterface.getByName("en0");
-			mSocket.joinGroup(socketAddress, networkInterface);
-		} catch (IOException e) {
-			System.err.println("Unable to join Group");
-		}
+		
 	}
 
 	public static void main(String[] args) throws IOException {	
@@ -68,7 +52,7 @@ public class DistributedChat {
 			if(cmd.equals("Control leave")){
 
 				// Multicast the leave message to peers.
-				String leaveMessage = "LEAVE$" + ID;
+				String leaveMessage = "LEAVE:" + ID;
 				byte[] buf = new byte[DatagramSize];
            		buf = leaveMessage.getBytes();
        	    	DatagramPacket packet = new DatagramPacket(buf, buf.length, MulticastAddress, Port);
@@ -81,9 +65,29 @@ public class DistributedChat {
 				return;
 			}
 			else if(cmd.equals("Control join")){
-				
+				// Creating the multicast socket
+				try {
+					mSocket = new MulticastSocket(Port);
+
+				} catch (Exception e) {
+					System.err.println("Exception in 1");
+				}
+				try { 
+					MulticastAddress = InetAddress.getByName("225.50.4.1");
+				} catch (java.net.UnknownHostException e) {
+					System.err.println("Unknown Host Reported");
+				}
+				try{
+					SocketAddress socketAddress = new InetSocketAddress(MulticastAddress, 9999);
+					NetworkInterface networkInterface = NetworkInterface.getByName("en0");
+					mSocket.joinGroup(socketAddress, networkInterface);
+				} catch (IOException e) {
+					System.err.println("Unable to join Group");
+				}
+
+
 				// Joining the chat. Multicast ID to Peers.
-				String joinMessage = "JOIN$" + ID;
+				String joinMessage = "JOIN:" + ID;
 				byte[] buf = new byte[DatagramSize];
            		buf = joinMessage.getBytes();
        	    	DatagramPacket packet = new DatagramPacket(buf, buf.length, MulticastAddress, Port);
@@ -127,10 +131,20 @@ public class DistributedChat {
 	       		mSocket.receive(multMessage); 
 	       		String multMessageStr = new String(multMessage.getData(), 0, multMessage.getLength());
 	       		System.out.println("Recvd message " + multMessageStr);
-	       		String[] fields = multMessageStr.split("$");
+	       		String[] fields = multMessageStr.split(":");
 	       		if(fields[0].equals("JOIN")) {
 	       			// New Peer joining. Update peer list.
-	       			addPeer(Integer.parseInt(fields[1]));
+	       			int id = Integer.parseInt(fields[1]);
+	       			if(id==ID) {} //Ignore
+	       			else {
+		       				addPeer(id);
+		       			try{
+		       				ChatInterface c = (ChatInterface) Naming.lookup("rmi://192.168.0.101:5000/" + Integer.parseInt(fields[1]));	
+		       				c.ackJoin(ID);
+		       			} catch (Exception e) {
+							// Peer not accessible
+						}
+					}
 	       		}
 	       		if(fields[0].equals("LEAVE")) {
 	       			//  Peer Leaving. Update peer list.
