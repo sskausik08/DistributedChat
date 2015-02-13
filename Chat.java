7ceaa54c4304ceb2a470711a18c3f0a55afea3bd
Kausik Subramanian <sskausik08@gmail.com>
2015-02-13 17:08:06 +0530

    Total Ordering Implemented. Testing to be done

--- Chat.java
+++ Chat.java
@@ -18,6 +18,18 @@ public class Chat extends UnicastRemoteObject implements ChatInterface {
 
 	
 	public void getMessage(String message, int msgID, int peerID) throws RemoteException {  // get Message from peer.
+		System.out.println("Executing getMessage");
 		DistributedChat.displayMessage(message, peerID);
 	}
+
+	public int sendInitialMessage(String message, int messageId) throws RemoteException {
+		System.out.println("Executing sendInitialMessage");
+		return DistributedChat.processInitial(message,messageId);
+	}
+	
+	public void sendFinalMessage(int msgID, int seqNo) throws RemoteException{
+		System.out.println("Executing sendFinalMessage");
+		DistributedChat.processFinal(msgID,seqNo);
+	}
+
 }
